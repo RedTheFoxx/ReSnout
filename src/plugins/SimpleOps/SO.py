b9a4352d2f0233c01da875768a83bc0f308b9e39
Red <morisseau.jeremy@gmail.com>
2024-12-18 18:36:16 +0100

    Added a demo !ping command to SImpleOps Plugin

--- src/plugins/SimpleOps/SO.py
+++ src/plugins/SimpleOps/SO.py
@@ -1,3 +1,13 @@
 """A ReSnout plugin. Check its bio in its .toml companion file."""
 
-pass
+import discord.ext.commands as commands
+
+class SimpleOps(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Test the gateway latency
+    @commands.command()
+    async def gw(self, ctx):
+        latency = round(self.bot.latency * 1000)  # Convert to ms and round
+        await ctx.send(f"Gateway latency: {latency}ms")
