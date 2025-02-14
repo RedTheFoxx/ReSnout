750266d9d4f41216c8d8b3686ac73df67d102312
Red <morisseau.jeremy@gmail.com>
2025-02-14 01:01:24 +0100

    docs: add Farkle rules and initiated the entrypoint file

---
+++ src/plugins/FarkleGame/FG.py
@@ -0,0 +1,8 @@
+"""Farkle Game is a dice game where the player rolls a combination of dice to try and get as high of a score as possible.
+It is designed to be played with 6 dice. Check the rules in the associated farklerules.md file."""
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+pass
